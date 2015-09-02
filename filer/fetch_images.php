bf6f3cbef6f1783fea76c32759c9b45f26c10910
Erik Elmehed <super_erik3@hotmail.com>
2015-09-02 14:51:54 +0200

    Paginering och bild-rutorna
    Fixat så att knapparna under bilderna nästa är klara, lite margins samt
    vit text på button. Pagineringen funkar.

--- filer/fetch_images.php
+++ filer/fetch_images.php
@@ -29,9 +29,11 @@ if(isset($_POST) && isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SER
 	while($row = $results->fetch_assoc()) {
 		echo '<div class="new_image">';
 		echo '<img src="' . $row["img_link"] . '"></img>';
-		echo '<p>votecount</p>';
+		echo '<div class="img_panel">';
+		echo '<div class="votecount"><div class="heart"></div><p>0</p></div>';
 		echo '<div class="share"></div>';
 		echo '<button>Rösta</button>';
+		echo '</div>';
 		echo '</div>';
 	}  
 	echo '</div>';
@@ -48,45 +50,30 @@ function paginate_function($item_per_page, $current_page, $total_records, $total
 {
     $pagination = '';
     if($total_pages > 0 && $total_pages != 1 && $current_page <= $total_pages){ //verify total pages and current page number
-        $pagination .= '<ul class="pagination">';
+        $pagination .= '<div class="pagination">';
         
-        $right_links    = $current_page + 3; 
-        $previous       = $current_page - 3; //previous link 
+        $right_links    = $current_page + 1; 
+        $previous       = $current_page - 1; //previous link 
         $next           = $current_page + 1; //next link
         $first_link     = true; //boolean var to decide our first link
         
         if($current_page > 1){
 			$previous_link = ($previous==0)?1:$previous;
-            $pagination .= '<li class="first"><a href="#" data-page="1" title="First">&laquo;</a></li>'; //first link
-            $pagination .= '<li><a href="#" data-page="'.$previous_link.'" title="Previous">&lt;</a></li>'; //previous link
-                for($i = ($current_page-2); $i < $current_page; $i++){ //Create left-hand side links
-                    if($i > 0){
-                        $pagination .= '<li><a href="#" data-page="'.$i.'" title="Page'.$i.'">'.$i.'</a></li>';
-                    }
-                }   
+            $pagination .= '<div class="previous"><a href="#" data-page="'.$previous_link.'" title="Previous">&laquo; FÖREGÅENDE</a></div>'; //previous link   
             $first_link = false; //set first link to false
-        }
-        
-        if($first_link){ //if current active page is first link
-            $pagination .= '<li class="first active">'.$current_page.'</li>';
-        }elseif($current_page == $total_pages){ //if it's the last active link
-            $pagination .= '<li class="last active">'.$current_page.'</li>';
-        }else{ //regular current link
-            $pagination .= '<li class="active">'.$current_page.'</li>';
         }
                 
         for($i = $current_page+1; $i < $right_links ; $i++){ //create right-hand side links
             if($i<=$total_pages){
-                $pagination .= '<li><a href="#" data-page="'.$i.'" title="Page '.$i.'">'.$i.'</a></li>';
+                $pagination .= '<div class="right_links"><a href="#" data-page="'.$i.'" title="Page '.$i.'">'.$i.'</a></div>';
             }
         }
-        if($current_page < $total_pages){ 
+        if($current_page <= $total_pages){ 
 				$next_link = ($i > $total_pages)? $total_pages : $i;
-                $pagination .= '<li><a href="#" data-page="'.$next_link.'" title="Next">&gt;</a></li>'; //next link
-                $pagination .= '<li class="last"><a href="#" data-page="'.$total_pages.'" title="Last">&raquo;</a></li>'; //last link
+                $pagination .= '<div class="page"><p>SIDA '.$current_page.' AV '.$total_pages.'</p></div><div class="next"><a href="#" data-page="'.$next_link.'" title="Next">NÄSTA &raquo;</a></div>'; //next link
         }
         
-        $pagination .= '</ul>'; 
+        $pagination .= '</div>'; 
     }
     return $pagination; //return pagination links
 }
