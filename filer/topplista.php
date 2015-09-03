e8fa25b567d1ebeffbb65816f5889f5ebd3797af
Erik Elmehed <super_erik3@hotmail.com>
2015-09-03 23:41:24 +0200

    Topplista funkar nu
    yey

--- filer/topplista.php
+++ filer/topplista.php
@@ -1,6 +1,6 @@
 <?php require 'header.php'; ?>
 <?php
-	$results = $conn->query("SELECT *, COUNT(votes) FROM images GROUP BY votes ORDER BY votes DESC LIMIT 9");
+	$results = $conn->query("SELECT * FROM images ORDER BY votes DESC LIMIT 9");
 	/*SELECT Group, COUNT(*) FROM table GROUP BY Group ORDER BY COUNT(*) DESC*/
 	//Display records fetched from database.
 	echo '<div id="top_images">';
@@ -9,9 +9,13 @@
 		echo '<div class="new_image">';
 		echo '<img src="' . $row["img_link"] . '"></img>';
 		echo '<div class="img_panel">';
-		echo '<div class="votecount"><div class="heart"></div><p>0</p></div>';
+		if($row['votes'] == 0){
+			echo '<div class="votecount"><div class="heart"></div><p>0</p></div>';
+		}else{
+			echo '<div class="votecount"><div class="redheart"></div><p>'. $row['votes'] .'</p></div>';
+		}
 		echo '<div class="share"></div>';
-		echo '<button>Rösta</button>';
+		echo '<button id="'. $row['img_id'] .'" class="btn_vote">Rösta</button>';
 		echo '</div>';
 		echo '</div>';
 	}
